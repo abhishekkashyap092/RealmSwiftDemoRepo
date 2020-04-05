06e9171305575eaf4168ebc18fef894d3571b048
Abhishek Kashyap <abhishekkashyap092@gmail.com>
2020-04-05 17:32:32 +0530

    Initial Commit

---
+++ RealmSwiftDemoRepo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealmSwiftDemoRepo
+//
+//  Created by Abhishek Kashyap on 05/04/20.
+//  Copyright © 2020 Abhishek Kashyap. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
